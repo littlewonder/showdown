9564e94fcb7b295236bc9d70f133e75f4db31ab4
Abhishek Sharma <hello.abhishek@outlook.com>
2017-04-16 03:21:55 +0530

    redudant code removed

--- script.js
+++ script.js
@@ -8,9 +8,7 @@ new Vue({
     el: '#ground',
     data: {
         playerHealth: 100,
-        cpuHealth: 100,
-        userwidth: 100,
-        cpuwidth: 100
+        cpuHealth: 100
     },
     methods: {
         reload: function () {
@@ -21,12 +19,10 @@ new Vue({
             if (this.cpuHealth > 0) {
                 let damage = getRandomInt(5, 15)
                 this.cpuHealth -= damage;
-                this.cpuwidth -= damage;
                 console.log("CPU LOST " + damage + " points");
                 if (this.playerHealth > 0) {
                     let damage2 = getRandomInt(5, 15);
                     this.playerHealth -= damage2;
-                    this.userwidth -= damage2;
                     console.log("YOU LOST " + damage2 + " points");
                 } else {
                     alert("YOU LOST NIGGA");
