5d33835e41ff4010e844cd3f59c69b215182c42c
Abhishek Sharma <hello.abhishek@outlook.com>
2017-04-16 03:13:53 +0530

    health bar added

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ new Vue({
     el: '#ground',
     data: {
         playerHealth: 100,
-        cpuHealth: 100
+        cpuHealth: 100,
+        userwidth: 100,
+        cpuwidth: 100
     },
     methods: {
         reload: function () {
@@ -19,11 +21,15 @@ new Vue({
             if (this.cpuHealth > 0) {
                 let damage = getRandomInt(5, 15)
                 this.cpuHealth -= damage;
+                this.cpuwidth -= damage;
+                console.log("CPU LOST " + damage + " points");
                 if (this.playerHealth > 0) {
-                    let damage2 = getRandomInt(4, 14);
+                    let damage2 = getRandomInt(5, 15);
                     this.playerHealth -= damage2;
+                    this.userwidth -= damage2;
+                    console.log("YOU LOST " + damage2 + " points");
                 } else {
-                    alert("GAME OVER");
+                    alert("YOU LOST NIGGA");
                 }
             } else {
                 alert("YOU WIN");
